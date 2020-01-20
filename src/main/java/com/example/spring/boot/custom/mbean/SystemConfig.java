56219d8c85baca26fb4c9f51964fdaa46ff498a5
amipatil <iamitpatil1993@gmail.com>
2020-01-20 09:34:43 +0530

    Customize Mbean ObjectName, attributes, operations using java annotations

--- src/main/java/com/example/spring/boot/custom/mbean/SystemConfig.java
+++ src/main/java/com/example/spring/boot/custom/mbean/SystemConfig.java
@@ -6,6 +6,7 @@ import javax.management.ObjectName;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,6 +18,8 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
+// We can use annotations to customize details of Mbean and attributes/operations it expose, See ManagedAttribute, ManagedOperation annotations.
+@ManagedResource(description = "SystemConfig managed bean", objectName = "com.example.spring.boot:name=SystemConfig")
 public class SystemConfig implements SystemConfigMBean {
 
 	private int threadCount = 10;
